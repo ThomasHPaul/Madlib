e2fa9eb4d7478dd34306612c607b0b208576e8f3
ThomasHPaul <tpaul19@wgu.edu>
2020-05-19 09:21:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Madlib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+
+            //write out a header
+
+            //ask player to enter words
+
+            //write out finished story
+
+            //keep window open and prompt for exit
+
+        }
+    }
+}
